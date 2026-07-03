ca63878be669fb97bf022e40283eac2c6795d372
Sofia <sofiaacero481@gmail.com>
2026-07-03 07:52:00 -0500

    Usar SQLite para simplificar despliegue inicial

--- sena/sena/settings.py
+++ sena/sena/settings.py
@@ -69,18 +69,11 @@ WSGI_APPLICATION = 'sena.wsgi.application'
 
 
 # ========== BASE DE DATOS MySQL ==========
+
 DATABASES = {
     'default': {
-        'ENGINE':   'django.db.backends.mysql',
-        'NAME':     os.environ.get('DB_NAME', 'acerautos_proyecto'),
-        'USER':     os.environ.get('DB_USER', 'root'),
-        'PASSWORD': os.environ.get('DB_PASSWORD', ''),
-        'HOST':     os.environ.get('DB_HOST', 'localhost'),
-        'PORT':     os.environ.get('DB_PORT', '3306'),
-        'OPTIONS': {
-            'charset': 'utf8mb4',
-            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
