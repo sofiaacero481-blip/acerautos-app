c3b4fa8ef774fa6386c394d574cc7f44d431b14e
Sofia <sofiaacero481@gmail.com>
2026-07-03 11:08:27 -0500

    mejora parcial

--- Acerautoos/sena/app/tests.py
+++ Acerautoos/sena/app/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from django.contrib.auth import get_user_model
-from .models import PerfilUsuario
+from usuario.models import PerfilUsuario
 
 User = get_user_model()
 
